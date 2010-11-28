e192b86a86d07aa37402470e8173c8f3b9e83c73
Tonyx <tonyx@tonyx-laptop.(none)>
2010-11-28 19:01:24 +0100

    mod

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -4,41 +4,42 @@ describe 'code kata'
     it 'for no match should mark to empty'
       mark("xxxx","rgby").should.be ""
     end
-    it 'for an imperfect match in first position should mark to m'
+    it 'for a perfect match in first position should mark to m'
       mark("yxxx","rgby").should.be "m"
     end
-    it 'for an imperfect match in second position should mark to m'
+    it 'asdlfjaslffor a perfect match in first position should mark to m'
+      countNonPositionals("yxxx","rgby").should.be 1
+    end
+    it 'sfklsdasdlfjaslffor a perfect match in first position should mark to m'
+	_.reduce(_.range(3),function(acc,n){return acc+"m";},"").should.be "mmm"
+      //countNonPositionals("yxxx","rgby").should.be 1
+    end
+    it 'for a perfect match in second position should mark to m'
       mark("xyxx","rgby").should.be "m"
     end
-    it 'for an imperfect match in third position should mark to m'
+    it 'for a perfect match in third position should mark to m'
       mark("xxyx","rgby").should.be "m"
     end
-    it 'for an imperfect match in third position should mark to m'
+    it 'for a perfect match in fourth position should mark to m'
       mark("xxxb","rgby").should.be "m"
     end
-    it 'for two imperfect match should mark to two m'
+    it 'for two matches should mark to two m'
       mark("yxxb","rgby").should.be "mm"
     end
-    it 'dont\'t  count twice the same match'
-      mark("yyxx","rgby").should.be "m"
+    it 'don\'t count twice the same match'
+      mark("bxxb","rgby").should.be "m"
     end
-    it 'dont\'t  count twice the same match'
-      mark("yyxx","rgby").should.be "m"
-    end
-    it 'for a perfect match in first position shoul mark to p'
+    it 'for perfect match in first position should mar to p'
       mark("rxxx","rgby").should.be "p"
     end
-    it 'for a perfect match in second position shoul mark to p'
+    it 'for perfect match in second position should mar to p'
       positionalMatch("xgxx","rgby").should.be "p"
     end
-    it 'for a perfect match in third position shoul mark to p'
+    it 'for perfect match in third position should mar to p'
       positionalMatch("xxbx","rgby").should.be "p"
     end
-    it 'for a perfect match in fourth position shoul mark to p'
-      positionalMatch("xxxy","rgby").should.be "p"
-    end
-    it 'two perfect matches should mark to two p'
-      positionalMatch("rxxy","rgby").should.be "pp"
+    it 'two perfect matche should mark to two p'
+      positionalMatch("rgxx","rgby").should.be "pp"
     end
   end
 end
