3f6d53e3f082e7a49ebf67c32e3ad6820fb03e60
Tonyx <tonyx@tonyx-laptop.(none)>
2010-11-29 01:27:15 +0100

    new ex

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -4,35 +4,35 @@ describe 'code kata'
     it 'for no matches should mark to empty'
       mark("xxxx","rgby").should.be ""
     end
-    it 'for imperfect match in first position should mark to m'
+    it 'for an imperfect match in first position should mark to m'
       mark("yxxx","rgby").should.be "m"
     end
-    it 'for imperfect match in second position should mark to m'
+    it 'for an imperfect match in second position should mark to m'
       mark("xyxx","rgby").should.be "m"
     end
-    it 'for imperfect match in third position should mark to m'
+    it 'for an imperfect match in third position should mark to m'
       mark("xxyx","rgby").should.be "m"
     end
-    it 'for imperfect match in fourth position should mark to m'
+    it 'for an imperfect match in fourth position should mark to m'
       mark("xxxb","rgby").should.be "m"
     end
-    it 'for two imperfect matches should mark to two m'
+    it 'for two matches shoulw mark to two m'
       mark("yxxb","rgby").should.be "mm"
     end
     it 'don\'t count twice the same match'
       mark("bxxb","rgby").should.be "m"
     end
-    it 'positional match in first position should mark to p'
+    it 'for a perfect match in first positions should mark to p'
       mark("rxxx","rgby").should.be "p"
     end
-    it 'positional match in second position should mark to p'
-      positionalMatch("xgxx","rgby").should.be "p"
+    it 'for a perfect match in second positions should mark to p'
+      mark("xgxx","rgby").should.be "p"
     end
-    it 'positional match in third position should mark to p'
-      positionalMatch("xxbx","rgby").should.be "p"
+    it 'for two perfect matches should mark to two p'
+      mark("rxbx","rgby").should.be "pp"
     end
-    it 'two perfect matches'
-      positionalMatch("rxbx","rgby").should.be "pp"
+    it 'for two perfect and two perfect should mark to ppmm'
+      mark("rybg","rgby").should.be "ppmm"
     end
   end
 end
