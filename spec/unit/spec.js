d0d30ed28fe652f4ce800454ef33964eedbb82e0
Tonyx <tonyx@tonyx-laptop.(none)>
2010-11-29 11:35:47 +0100

    xxx

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -4,35 +4,23 @@ describe 'code kata'
     it 'for no matches should mark to empty'
       mark("xxxx","rgby").should.be ""
     end
-    it 'for an imperfect match in first position should mark to m'
+    it 'for imperfect match first position should mark to m'
       mark("yxxx","rgby").should.be "m"
     end
-    it 'for an imperfect match in second position should mark to m'
+    it 'for imperfect match second position should mark to m'
       mark("xyxx","rgby").should.be "m"
     end
-    it 'for an imperfect match in third position should mark to m'
+    it 'for imperfect match third position should mark to m'
       mark("xxyx","rgby").should.be "m"
     end
-    it 'for an imperfect match in fourth position should mark to m'
+    it 'for imperfect match fourth position should mark to m'
       mark("xxxb","rgby").should.be "m"
     end
-    it 'for two matches shoulw mark to two m'
+    it 'two perfect match gets two  m'
       mark("yxxb","rgby").should.be "mm"
     end
     it 'don\'t count twice the same match'
       mark("bxxb","rgby").should.be "m"
-    end
-    it 'for a perfect match in first positions should mark to p'
-      mark("rxxx","rgby").should.be "p"
-    end
-    it 'for a perfect match in second positions should mark to p'
-      mark("xgxx","rgby").should.be "p"
-    end
-    it 'for two perfect matches should mark to two p'
-      mark("rxbx","rgby").should.be "pp"
-    end
-    it 'for two perfect and two perfect should mark to ppmm'
-      mark("rybg","rgby").should.be "ppmm"
     end
   end
 end
