e3167cd3f03b7672e9dce9f2fe4a4380df2cf028
Tonyx <tonyx@tonyx-laptop.(none)>
2010-12-05 02:17:30 +0100

    lst

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,41 +1,34 @@
-
 describe 'code kata'
   describe 'codebreaker'
     it 'for no matches should mark to empty'
       mark("xxxx","rgby").should.be ""
     end
-    it 'for an imperfect match in first position should mark to m'
+    it 'for an imperfect match in first positio should mark to m'
       mark("yxxx","rgby").should.be "m"
     end
-    it 'for an imperfect match in second position should mark to m'
+    it 'for an imperfect match in second positio should mark to m'
       mark("xyxx","rgby").should.be "m"
     end
-    it 'for an imperfect match in third position should mark to m'
-      mark("xxyx","rgby").should.be "m"
-    end
-    it 'for an imperfect match in fourth position should mark to m'
-      mark("xxxb","rgby").should.be "m"
-    end
     it 'for two matches should mark to two m'
-      mark("bxxg","rgby").should.be "mm"
+      mark("byxx","rgby").should.be "mm"
     end
-    it 'for a repeated match should not count them twice'
-      mark("bxxb","rgby").should.be "m"
+    it 'for same match should count it just once'
+      mark("yyxx","rgby").should.be "m"
     end
-    it 'for perfect match in firstr position should mark to p'
+    it 'for a perfect match in first position should mark to p'
       mark("rxxx","rgby").should.be "p"
     end
-    it 'for perfect match in second position should mark to p'
+    it 'for a perfect match in second position should mark to p'
       mark("xgxx","rgby").should.be "p"
     end
-    it 'for two  perfect matches should mark to two p'
+    it 'for two matches should mark to two p'
       mark("rgxx","rgby").should.be "pp"
     end
-    it 'two perfect matches and two imperfect matches should mark to ppmm'
+    it 'for two perfect matches and two impefect should  mark to two p and two m'
       mark("rgyb","rgby").should.be "ppmm"
+    end
+    it 'for two imperfect matches equals between them, mark them with two m'
+      mark("ggxx","rbgg").should.be "mm"
     end
   end
 end
-
-
-
